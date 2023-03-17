e4f89660a6592341cafe41d1ec717ee43c6040a2
Zixin Shen <32036842+szxxw@users.noreply.github.com>
2023-03-17 17:30:56 +0800

    Add files via upload

---
+++ discussion_7.py
@@ -0,0 +1,334 @@
+class Email:
+    """Every email object has 3 instance attributes: the
+    message, the sender name, and the recipient name.
+    >>> email = Email('hello', 'Alice', 'Bob')
+    >>> email.msg
+    'hello'
+    >>> email.sender_name
+    'Alice'
+    >>> email.recipient_name
+    'Bob'
+    """
+    def __init__(self, msg, sender_name, recipient_name):
+        "*** YOUR CODE HERE ***"
+        self.msg = msg
+        self.sender_name = sender_name
+        self.recipient_name = recipient_name
+        
+class Server:
+    """Each Server has an instance attribute clients, which
+    is a dictionary that associates client names with
+    client objects.
+    """
+    def __init__(self):
+        self.clients = {}
+
+    def send(self, email):
+        """Take an email and put it in the inbox of the client
+        it is addressed to.
+        """
+        "*** YOUR CODE HERE ***"
+        client = self.clients[email.recipient_name]
+        client.receive(email)
+
+    def register_client(self, client, client_name):
+        """Takes a client object and client_name and adds them
+        to the clients instance attribute.
+        """
+        "*** YOUR CODE HERE ***"
+        self.clients[client_name] = client
+
+class Client:
+    """Every Client has instance attributes name (which is
+    used for addressing emails to the client), server
+    (which is used to send emails out to other clients), and
+    inbox (a list of all emails the client has received).
+    >>> s = Server()
+    >>> a = Client(s, 'Alice')
+    >>> b = Client(s, 'Bob')
+    >>> a.compose('Hello, World!', 'Bob')
+    >>> b.inbox[0].msg
+    'Hello, World!'
+    >>> a.compose('CS 61A Rocks!', 'Bob')
+    >>> len(b.inbox)
+    2
+    >>> b.inbox[1].msg
+    'CS 61A Rocks!'
+    """
+    def __init__(self, server, name):
+        self.inbox = []
+        "*** YOUR CODE HERE ***"
+        self.name = name
+        self.server = server
+        self.server.register_client(self, self.name)
+
+    def compose(self, msg, recipient_name):
+        """Send an email with the given message msg to the
+        given recipient client.
+        """
+        "*** YOUR CODE HERE ***"
+        email = Email(msg, self.name, recipient_name)
+        self.server.send(email)
+       
+
+
+    def receive(self, email):
+        """Take an email and add it to the inbox of this
+        client.
+        """
+        "*** YOUR CODE HERE ***"
+        self.inbox.append(email)
+
+# q3 cat
+class Pet:
+
+    def __init__(self, name, owner):
+        self.is_alive = True    # It's alive!!!
+        self.name = name
+        self.owner = owner
+
+    def eat(self, thing):
+        print(self.name + " ate a " + str(thing) + "!")
+
+    def talk(self):
+        print(self.name)
+
+class Dog(Pet):
+
+    def talk(self):
+        super().talk()
+        print('This Dog says woof!')
+
+class Cat(Pet):
+
+    def __init__(self, name, owner, lives=9):
+        "*** YOUR CODE HERE ***"
+        super().__init__(name, owner)
+        self.lives = lives
+
+    def talk(self):
+        """Print out a cat's greeting.
+
+        >>> Cat('Thomas', 'Tammy').talk()
+        Thomas says meow!
+        """
+        "*** YOUR CODE HERE ***"
+        print(self.name + ' says meow!')
+
+    def lose_life(self):
+        """Decrements a cat's life by 1. When lives reaches zero,
+        is_alive becomes False. If this is called after lives has
+        reached zero, print 'This cat has no more lives to lose.'
+        """
+        "*** YOUR CODE HERE ***"
+        self.lives -= 1
+        if self.lives == 0:
+            super().is_alive = False
+            print('This cat has no more lives to lose.')
+
+class NoisyCat(Cat): # Fill me in!
+    """A Cat that repeats things twice."""
+    def __init__(self, name, owner, lives=9):
+        # Is this method necessary? Why or why not?
+        # not necessary 
+        "*** YOUR CODE HERE ***"
+        super().__init__(name, owner, lives)
+
+    def talk(self):
+        """Talks twice as much as a regular cat.
+        >>> NoisyCat('Magic', 'James').talk()
+        Magic says meow!
+        Magic says meow!
+        """
+        "*** YOUR CODE HERE ***"
+        super().talk()
+        super().talk()
+
+class Cat:
+    def __init__(self, name, owner, lives=9):
+        self.is_alive = True
+        self.name = name
+        self.owner = owner
+        self.lives = lives
+
+    def talk(self):
+        return self.name + ' says meow!'
+
+    @classmethod
+    def cat_creator(cls, owner):
+        """
+        Returns a new instance of a Cat.
+
+        This instance's name is "[owner]'s Cat", with 
+        [owner] being the name of its owner.
+
+        >>> cat1 = Cat.cat_creator("Bryce")
+        >>> isinstance(cat1, Cat)
+        True
+        >>> cat1.owner
+        'Bryce'
+        >>> cat1.name
+        "Bryce's Cat"
+        >>> cat2 = Cat.cat_creator("Tyler")
+        >>> cat2.owner
+        'Tyler'
+        >>> cat2.name
+        "Tyler's Cat"
+        """
+        name = owner + "'s Cat"
+        return cls(name, owner)
+
+#q4 keyboard
+class Button:
+    def __init__(self, pos, key):
+        self.pos = pos
+        self.key = key
+        self.times_pressed = 0
+
+class Keyboard:
+    """A Keyboard stores an arbitrary number of Buttons in a dictionary. 
+    Each dictionary key is a Button's position, and each dictionary
+    value is the corresponding Button.
+    >>> b1, b2 = Button(5, "H"), Button(7, "I")
+    >>> k = Keyboard(b1, b2)
+    >>> k.buttons[5].key
+    'H'
+    >>> k.press(7)
+    'I'
+    >>> k.press(0) # No button at this position
+    ''
+    >>> k.typing([5, 7])
+    'HI'
+    >>> k.typing([7, 5])
+    'IH'
+    >>> b1.times_pressed
+    2
+    >>> b2.times_pressed
+    3
+    """
+    def __init__(self, *args):
+        self.buttons = {}
+        for i in args:
+            self.buttons[i.pos] = i
+    def press(self, pos):
+        """Takes in a position of the button pressed, and
+        returns that button's output."""
+        if pos in self.buttons:
+            self.buttons[pos].times_pressed += 1
+            return self.buttons[pos].key
+        else:
+            return ''
+
+    def typing(self, typing_input):
+        """Takes in a list of positions of buttons pressed, and
+        returns the total output."""
+        result = ''
+        for _ in typing_input:
+            result += self.press(_)
+        return result
+# Q4 relay
+
+
+class TeamMember:
+    """
+    >>> adder = TeamMember(lambda x: x + 1) # team member at front 
+    >>> adder2 = TeamMember(lambda x: x + 2, adder) # team member 2
+    >>> multiplier = TeamMember(lambda x: x * 5, adder2) # team member 3
+    >>> adder.relay_history() # relay history starts off as empty
+    []
+    >>> adder.relay_calculate(5) # 5 + 1
+    6
+    >>> adder2.relay_calculate(5) # (5 + 1) + 2
+    8
+    >>> multiplier.relay_calculate(5) # (((5 + 1) + 2) * 5)
+    40
+    >>> multiplier.relay_history() # history of answers from the most recent relay multiplier participated in
+    [6, 8, 40]
+    >>> adder.relay_history()
+    [6]
+    >>> multiplier.relay_calculate(4) # (((4 + 1) + 2) * 5)
+    35
+    >>> multiplier.relay_history()
+    [5, 7, 35]
+    >>> adder.relay_history() # adder participated most recently in multiplier.relay_calculate(4), where it gave the answer 5
+    [5]
+    >>> adder.relay_calculate(1)
+    2
+    >>> adder.relay_history() # adder participated most recently in adder.relay_calculate(1), where it gave the answer 2
+    [2]
+    >>> multiplier.relay_history() # but the most relay multiplier participated in is still multiplier.relay_calculate(4)
+    [5, 7, 35]
+    """
+    
+    def __init__(self, operation, prev_member=None):
+        """
+        A TeamMember object is instantiated by taking in an `operation`
+        and a TeamMember object `prev_member`, which is the team member
+        who "sits in front of" this current team member. A TeamMember also
+        tracks a `history` list, which contains the answers given by
+        each individual team member.
+        """
+        self.history = []
+        self.operation = operation
+        self.prev_member = prev_member
+
+    def relay_calculate(self, x):
+        """
+        The relay_calculate method takes in a number `x` and performs a
+        relay by passing in `x` to the first team member's `operation`.
+        Then, that answer is passed to the next member's operation, etc. until
+        we get to the current TeamMember, in which case we return the
+        final answer, `result`. 
+        """
+        if self.prev_member is None:
+            result = self.operation(x)
+            self.history = [result]
+        else:
+            result = self.operation(self.prev_member.relay_calculate(x))
+            self.history = self.prev_member.history + [result]
+
+        return result
+
+    def relay_history(self):
+        """
+        Returns a list of the answers given by each team member in the
+        most recent relay the current TeamMember has participated in.
+        """
+        return self.history.copy()
+
+
+class Cat:
+    def __init__(self, name, owner, lives=9):
+        self.is_alive = True
+        self.name = name
+        self.owner = owner
+        self.lives = lives
+
+    def talk(self):
+        return self.name + ' says meow!'
+
+    @classmethod
+    def cat_creator(cls, owner):
+        """
+        Returns a new instance of a Cat.
+
+        This instance's name is "[owner]'s Cat", with 
+        [owner] being the name of its owner.
+
+        >>> cat1 = Cat.cat_creator("Bryce")
+        >>> isinstance(cat1, Cat)
+        True
+        >>> cat1.owner
+        'Bryce'
+        >>> cat1.name
+        "Bryce's Cat"
+        >>> cat2 = Cat.cat_creator("Tyler")
+        >>> cat2.owner
+        'Tyler'
+        >>> cat2.name
+        "Tyler's Cat"
+        """
+        name = owner + "'s Cat"
+        return cls(name, owner)
+
+
