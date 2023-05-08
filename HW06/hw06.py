677a415b1e4f9a2100a6d80b600fdc046ea9511d
Zixin Shen <32036842+szxxw@users.noreply.github.com>
2023-05-08 22:30:43 +0800

    Update hw06.py

--- HW06/hw06.py
+++ HW06/hw06.py
@@ -123,7 +123,28 @@ class VendingMachine:
             self.balance += amount
             return 'Current balance: ${}'.format(self.balance)
         
-    
+def store_digits(n):
+    """Stores the digits of a positive number n in a linked list.
+
+    >>> s = store_digits(1)
+    >>> s
+    Link(1)
+    >>> store_digits(2345)
+    Link(2, Link(3, Link(4, Link(5))))
+    >>> store_digits(876)
+    Link(8, Link(7, Link(6)))
+    >>> # a check for restricted functions
+    >>> import inspect, re
+    >>> cleaned = re.sub(r"#.*\\n", '', re.sub(r'"{3}[\s\S]*?"{3}', '', inspect.getsource(store_digits)))
+    >>> print("Do not use str or reversed!") if any([r in cleaned for r in ["str", "reversed"]]) else None
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
+    """
+    "*** YOUR CODE HERE ***"
+    result = Link.empty
+    while n > 0:
+        result = Link(n % 10, result)
+        n = n //10
+    return result
 
 
 def make_test_random():
