ecbdc8dd38d3b7fa78d12b31bcc96d7bfa0d35e0
Zixin Shen <32036842+szxxw@users.noreply.github.com>
2023-05-14 11:33:58 +0800

    Add files via upload

--- HW06/hw06.py
+++ HW06/hw06.py
@@ -33,9 +33,12 @@ class Mint:
         self.update()
 
     def create(self, coin):
+        "*** YOUR CODE HERE ***"
         return coin(self.year)
 
+
     def update(self):
+        "*** YOUR CODE HERE ***"
         self.year = Mint.present_year
 
 
@@ -46,6 +49,7 @@ class Coin:
         self.year = year
 
     def worth(self):
+        "*** YOUR CODE HERE ***"
         return self.cents + max(0, Mint.present_year - self.year - 50)
 
 
@@ -57,268 +61,95 @@ class Dime(Coin):
     cents = 10
 
 
-class VendingMachine:
-    """A vending machine that vends some product for some price.
+def store_digits(n):
+    """Stores the digits of a positive number n in a linked list.
 
-    >>> v = VendingMachine('candy', 10)
-    >>> v.vend()
-    'Nothing left to vend. Please restock.'
-    >>> v.add_funds(15)
-    'Nothing left to vend. Please restock. Here is your $15.'
-    >>> v.restock(2)
-    'Current candy stock: 2'
-    >>> v.vend()
-    'Please add $10 more funds.'
-    >>> v.add_funds(7)
-    'Current balance: $7'
-    >>> v.vend()
-    'Please add $3 more funds.'
-    >>> v.add_funds(5)
-    'Current balance: $12'
-    >>> v.vend()
-    'Here is your candy and $2 change.'
-    >>> v.add_funds(10)
-    'Current balance: $10'
-    >>> v.vend()
-    'Here is your candy.'
-    >>> v.add_funds(15)
-    'Nothing left to vend. Please restock. Here is your $15.'
-
-    >>> w = VendingMachine('soda', 2)
-    >>> w.restock(3)
-    'Current soda stock: 3'
-    >>> w.restock(3)
-    'Current soda stock: 6'
-    >>> w.add_funds(2)
-    'Current balance: $2'
-    >>> w.vend()
-    'Here is your soda.'
+    >>> s = store_digits(1)
+    >>> s
+    Link(1)
+    >>> store_digits(2345)
+    Link(2, Link(3, Link(4, Link(5))))
+    >>> store_digits(876)
+    Link(8, Link(7, Link(6)))
+    >>> # a check for restricted functions
+    >>> import inspect, re
+    >>> cleaned = re.sub(r"#.*\\n", '', re.sub(r'"{3}[\s\S]*?"{3}', '', inspect.getsource(store_digits)))
+    >>> print("Do not use str or reversed!") if any([r in cleaned for r in ["str", "reversed"]]) else None
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
     """
-    def __init__(self, product, price):
-        self.product = product
-        self.price = price
-        self.stock = 0
-        self.balance = 0
-    def restock(self,amount):
-        self.stock += amount
-        return 'Current {} stock: {}'.format(self.product,self.stock)
-    def vend(self):
-        if self.stock == 0:
-            return 'Nothing left to vend. Please restock.'
-        elif self.balance < self.price:
-            return 'Please add ${} more funds.'.format(self.price-self.balance)
-        elif self.balance == self.price:
-            self.stock -= 1
-            self.balance = 0
-            return 'Here is your {}.'.format(self.product)
-        else:
-            self.stock -= 1
-            change = self.balance - self.price
-            self.balance = 0
-            return 'Here is your {} and ${} change.'.format(self.product,change)
-    def add_funds(self,amount):
-        if self.stock == 0:
-            return 'Nothing left to vend. Please restock. Here is your ${}.'.format(amount)
-        else:
-            self.balance += amount
-            return 'Current balance: ${}'.format(self.balance)
-        
-    
+    "*** YOUR CODE HERE ***"
+    result = Link.empty
+    while n > 0:
+        result = Link(n % 10, result)
+        n = n //10
+    return result
 
 
-def make_test_random():
-    """A deterministic random function that cycles between
-    [0.0, 0.1, 0.2, ..., 0.9] for testing purposes.
+def deep_map_mut(func, lnk):
+    """Mutates a deep link lnk by replacing each item found with the
+    result of calling func on the item.  Does NOT create new Links (so
+    no use of Link's constructor).
 
-    >>> random = make_test_random()
-    >>> random()
-    0.0
-    >>> random()
-    0.1
-    >>> random2 = make_test_random()
-    >>> random2()
-    0.0
+    Does not return the modified Link object.
+
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
+    >>> # Disallow the use of making new Links before calling deep_map_mut
+    >>> Link.__init__, hold = lambda *args: print("Do not create any new Links."), Link.__init__
+    >>> try:
+    ...     deep_map_mut(lambda x: x * x, link1)
+    ... finally:
+    ...     Link.__init__ = hold
+    >>> print(link1)
+    <9 <16> 25 36>
     """
-    rands = [x / 10 for x in range(10)]
-
-    def random():
-        rand = rands[0]
-        rands.append(rands.pop(0))
-        return rand
-    return random
-
-
-random = make_test_random()
-
-# Phase 1: The Player Class
+    "*** YOUR CODE HERE ***"
+    if lnk is Link.empty:
+        return
+    elif isinstance(lnk.first, Link):
+        deep_map_mut(func, lnk.first)
+    else:
+        lnk.first = func(lnk.first)
+    deep_map_mut(func, lnk.rest)
 
 
-class Player:
+
+   
+
+
+def two_list(vals, counts):
     """
-    >>> random = make_test_random()
-    >>> p1 = Player('Hill')
-    >>> p2 = Player('Don')
-    >>> p1.popularity
-    100
-    >>> p1.debate(p2)  # random() should return 0.0
-    >>> p1.popularity
-    150
-    >>> p2.popularity
-    100
-    >>> p2.votes
-    0
-    >>> p2.speech(p1)
-    >>> p2.votes
-    10
-    >>> p2.popularity
-    110
-    >>> p1.popularity
-    135
+    Returns a linked list according to the two lists that were passed in. Assume
+    vals and counts are the same size. Elements in vals represent the value, and the
+    corresponding element in counts represents the number of this value desired in the
+    final linked list. Assume all elements in counts are greater than 0. Assume both
+    lists have at least one element.
 
-
-    >>> # Additional correctness tests
-    >>> p1.speech(p2)
-    >>> p1.votes
-    13
-    >>> p1.popularity
-    148
-    >>> p2.votes
-    10
-    >>> p2.popularity
-    99
-    >>> for _ in range(4):  # 0.1, 0.2, 0.3, 0.4
-    ...     p1.debate(p2)
-    >>> p2.debate(p1)
-    >>> p2.popularity
-    49
-    >>> p2.debate(p1)
-    >>> p2.popularity
-    0
-
+    >>> a = [1, 3, 2]
+    >>> b = [1, 1, 1]
+    >>> c = two_list(a, b)
+    >>> c
+    Link(1, Link(3, Link(2)))
+    >>> a = [1, 3, 2]
+    >>> b = [2, 2, 1]
+    >>> c = two_list(a, b)
+    >>> c
+    Link(1, Link(1, Link(3, Link(3, Link(2)))))
     """
-
-    def __init__(self, name):
-        self.name = name
-        self.votes = 0
-        self.popularity = 100
-
-    def debate(self, other):
-        prob = max(0.1, self.popularity / (self.popularity + other.popularity))
-        if random() < prob:
-            self.popularity += 50
-        else:
-            self.popularity = max(0, self.popularity - 50)
-
-    def speech(self, other):
-        self.votes += self.popularity // 10
-        self.popularity += self.popularity // 10
-        other.popularity -= other.popularity // 10
-
-    def choose(self, other):
-        return self.speech
-
-
-# Phase 2: The Game Class
-class Game:
-    """
-    >>> p1, p2 = Player('Hill'), Player('Don')
-    >>> g = Game(p1, p2)
-    >>> winner = g.play()
-    >>> p1 is winner
-    True
-
-
-    >>> # Additional correctness tests
-    >>> winner is g.winner()
-    True
-    >>> g.turn
-    10
-    >>> p1.votes = p2.votes
-    >>> print(g.winner())
-    None
-
-    """
-
-    def __init__(self, player1, player2):
-        self.p1 = player1
-        self.p2 = player2
-        self.turn = 0
-
-    def play(self):
-        while not self.game_over():
-            if self.turn % 2 == 0:
-                curr, other = self.p1, self.p2
-            else:
-                curr, other = self.p2, self.p1
-            curr.choose(other)(other)
-            self.turn += 1
-        return self.winner()
-
-    def game_over(self):
-        return max(self.p1.votes, self.p2.votes) >= 50 or self.turn >= 10
-
-    def winner(self):
-        if self.p1.votes > self.p2.votes:
-            return self.p1
-        elif self.p2.votes > self.p1.votes:
-            return self.p2
-        else:
-            return None
-
-
-# Phase 3: New Players
-class AggressivePlayer(Player):
-    """
-    >>> random = make_test_random()
-    >>> p1, p2 = AggressivePlayer('Don'), Player('Hill')
-    >>> g = Game(p1, p2)
-    >>> winner = g.play()
-    >>> p1 is winner
-    True
-
-
-    >>> # Additional correctness tests
-    >>> p1.popularity = p2.popularity
-    >>> p1.choose(p2) == p1.debate
-    True
-    >>> p1.popularity += 1
-    >>> p1.choose(p2) == p1.debate
-    False
-    >>> p2.choose(p1) == p2.speech
-    True
-
-    """
-
-    def choose(self, other):
-        if self.popularity <= other.popularity:
-            return self.debate
-        else:
-            return self.speech
-
-
-class CautiousPlayer(Player):
-    """
-    >>> random = make_test_random()
-    >>> p1, p2 = CautiousPlayer('Hill'), AggressivePlayer('Don')
-    >>> p1.popularity = 0
-    >>> p1.choose(p2) == p1.debate
-    True
-    >>> p1.popularity = 1
-    >>> p1.choose(p2) == p1.debate
-    False
-
-
-    >>> # Additional correctness tests
-    >>> p2.choose(p1) == p2.speech
-    True
-
-    """
-
-    def choose(self, other):
-        if self.popularity == 0:
-            return self.debate
-        else:
-            return self.speech
+    "*** YOUR CODE HERE ***"
+    result = Link(None)
+    p = result
+    length = len(counts)
+    for i in range(length):
+        repeat = counts[i]
+        val = vals[i]
+        for x in range(repeat):
+            p.rest = Link(val)
+            print(p)
+            p = p.rest
+            print(p)
+            print('this')
+            print(result)
+    return result.rest
 
 
 class VirFib():
@@ -347,12 +178,113 @@ class VirFib():
         self.value = value
 
     def next(self):
-        if self.value == 0:
-            result = VirFib(1)
-        else:
-            result = VirFib(self.value + self.previous)
-        result.previous = self.value
-        return result
+        "*** YOUR CODE HERE ***"
 
     def __repr__(self):
         return "VirFib object, value " + str(self.value)
+
+
+def is_bst(t):
+    """Returns True if the Tree t has the structure of a valid BST.
+
+    >>> t1 = Tree(6, [Tree(2, [Tree(1), Tree(4)]), Tree(7, [Tree(7), Tree(8)])])
+    >>> is_bst(t1)
+    True
+    >>> t2 = Tree(8, [Tree(2, [Tree(9), Tree(1)]), Tree(3, [Tree(6)]), Tree(5)])
+    >>> is_bst(t2)
+    False
+    >>> t3 = Tree(6, [Tree(2, [Tree(4), Tree(1)]), Tree(7, [Tree(7), Tree(8)])])
+    >>> is_bst(t3)
+    False
+    >>> t4 = Tree(1, [Tree(2, [Tree(3, [Tree(4)])])])
+    >>> is_bst(t4)
+    True
+    >>> t5 = Tree(1, [Tree(0, [Tree(-1, [Tree(-2)])])])
+    >>> is_bst(t5)
+    True
+    >>> t6 = Tree(1, [Tree(4, [Tree(2, [Tree(3)])])])
+    >>> is_bst(t6)
+    True
+    >>> t7 = Tree(2, [Tree(1, [Tree(5)]), Tree(4)])
+    >>> is_bst(t7)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
