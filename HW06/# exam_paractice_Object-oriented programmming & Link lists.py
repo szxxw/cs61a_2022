4b2dd00a58819f95daaa7c7d4650c1813d6414d8
Zixin Shen <32036842+szxxw@users.noreply.github.com>
2023-05-29 10:43:33 +0800

    Update # exam_paractice_Object-oriented programmming & Link lists.py

--- HW06/# exam_paractice_Object-oriented programmming & Link lists.py
+++ HW06/# exam_paractice_Object-oriented programmming & Link lists.py
@@ -361,4 +361,30 @@ def is_minimal(state_codes, k):
     in_favor_no_smallest = sum(votes_in_favor) - min(votes_in_favor) 
     return win_margin(in_favor_no_smallest) < k
 
+# 2018 fall mt2 q6: Dr.Frankenlink
+
+
+def replace(s, t, i, j):
+    """Replace the slice of s from i to j with t.
+    >>> s, t = Link(3, Link(4, Link(5, Link(6, Link(7))))), Link(0, Link(1, Link(2)))
+    >>> replace(s, t, 2, 4)
+    >>> print(s)
+    <3, 4, 0, 1, 2, 7>
+    >>> t.rest.first = 8
+    >>> print(s)
+    <3, 4, 0, 8, 2, 7>
+    """
+    assert s is not Link.empty and t is not Link.empty and i > 0 and i < j
+    if i > 1:
+        replace(s.rest, t, i-1, j-1)
+    else:
+        for k in range(j-i):
+            s.rest = s.rest.resst
+        end = t
+        while end.rest is not Link.empty:
+            end = end.rest
+        s.rest, end.rest = t, s.rest
+
+
+# spring 2017 mt1 q5 insert
 
