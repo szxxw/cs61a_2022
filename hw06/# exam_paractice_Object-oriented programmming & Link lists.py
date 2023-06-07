0796baa195fc9789cbe2898831df2bf0e84ce2fe
Zixin Shen <32036842+szxxw@users.noreply.github.com>
2023-06-07 17:15:57 +0800

    Update # exam_paractice_Object-oriented programmming & Link lists.py

--- hw06/# exam_paractice_Object-oriented programmming & Link lists.py
+++ hw06/# exam_paractice_Object-oriented programmming & Link lists.py
@@ -412,4 +412,96 @@ def replace(s, t, i, j):
         # This effectively stitches the remaining elements of s onto the end of t, maintaining the elements of s that were after the replaced slice.
         s.rest, end.rest = t, s.rest
 
+# sp17 mt1 05 insert
+def link_insert(lnklst, value, before):
+    """Return a linked list identical to LNKLST, but with VALUE inserted just
+    before the first occurrence of BEFORE in the list, if any. The returned
+    list is identical to LNKLST if BEFORE does not occur in LNKLST.
+    The operation is non-destructive.
+    >>> L = Link(2, Link(3, Link(7, Link(1))))
+    >>> print(L)
+    <2 3 7 1>
+    >>> Q = link_insert(L, 19, 7)
+    >>> print(Q)
+    <2 3 19 7 1>
+    >>> print(link_insert(L, 19, 20))
+    <2 3 7 1>
+    """
+    if before == lnklst.first:
+        return Link(value, lnklst)
+    elif lnklst.rest == Link.empty:
+        return lnklst
+    else:
+        return Link(lnklst.first, link_insert(lnklst.rest, value, before))
+# fa20 final 03 college party
+class State:
+    electors = {}
+    def __init__(self, code, electors):
+        self.code = code
+        self.electors = electors
+        State.electors[code] = electors
+    battleground = [State('AZ', 11), State('PA', 20), State('NV', 6),
+    State('GA', 16), State('WI', 10), State('MI', 16)]
+
+def print_all(s):
+    for x in s:
+        print(x)
+
+ 
+def wins(states, k):
+    """Yield each linked list of two-letter state codes that describes a win by at least k.
+    >>> print_all(wins(battleground, 50))
+    <AZ PA NV GA WI MI>
+    <AZ PA NV GA MI>
+    <AZ PA GA WI MI>
+    <PA NV GA WI MI>
+    >>> print_all(wins(battleground, 75))
+    <AZ PA NV GA WI MI>
+    """
+    if k <= 0 and not states:
+        yield Link.empty
+    if states:
+        first = states[0].electors
+        for win in wins(states[0].code, k - first):
+            yield Link(first, win)
+    yield from wins(states[1:], k + first)
+
+def must_win(states, k):
+    """List all states that must be won in every scenario that wins by k.
+    >>> must_win(battleground, 50)
+    ['PA', 'GA', 'MI']
+    >>> must_win(battleground, 75)
+    ['AZ', 'PA', 'NV', 'GA', 'WI', 'MI']
+    """
+    def contains(s, x):
+        """Return whether x is a value in linked list s."""
+        return (s is not Link.empty) and (s.first == x or contains(s.rest, x))
+        # (a) (b)
+    return [s.code for s in states if all([contains(w, s.code) for w in wins(states, k)])]
+
+def is_minimal(state_codes, k):
+    """Return whether a non-empty list of state_codes describes a minimal win by
+    at least k.
+    >>> is_minimal(['AZ', 'NV', 'GA', 'WI'], 0) # Every state is necessary
+    True
+    >>> is_minimal(['AZ', 'GA', 'WI'], 0) # Not a win
+    False
+    >>> is_minimal(['AZ', 'NV', 'PA', 'WI'], 0) # NV is not necessary
+    False
+    >>> is_minimal(['AZ', 'PA', 'WI'], 0) # Every state is necessary
+    True
+    """
+    assert state_codes, 'state_codes must not be empty'
+    votes_in_favor = [State.electors[code] for code in state_codes]
+    total_possible_votes = sum(State.electors.values())
+
+    def win_margin(n):
+        "Margin of victory if n votes are in favor and the rest are against."
+        return n - (total_possible_votes - n)
+    
+    if win_margin(sum(votes_in_favor)) < k:
+        return False # Not a win
+    in_favor_no_smallest = sum(votes_in_favor)- min(votes_in_favor)
+    # (c)
+    return win_margin(in_favor_no_smallest) < k
 
