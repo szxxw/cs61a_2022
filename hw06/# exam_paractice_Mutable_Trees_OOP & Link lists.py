b1a9b6912f811489b429e58b662dce6b9bcffa20
Zixin Shen <32036842+szxxw@users.noreply.github.com>
2023-06-08 22:23:35 +0800

    Update # exam_paractice_Mutable_Trees_OOP & Link lists.py

--- hw06/# exam_paractice_Mutable_Trees_OOP & Link lists.py
+++ hw06/# exam_paractice_Mutable_Trees_OOP & Link lists.py
@@ -553,4 +553,36 @@ def pruned(t):
     
 # sp19 mt2 06 Tries this
   
+# sp19 mt2 06 Tries this
+  
+def make_trie(words):
+    """ Makes a tree where every node is a letter of a word.
+    All words end as a leaf of the tree.
+    words is given as a list of strings.
+    """
+    trie = Tree('')
+    for word in words:
+        add_word(trie, word)
+    return trie
 
+def add_word(trie, word):
+    if word == '':
+        return 
+    branch = None
+    for b in trie.branches:
+        if b.label == word[0]:
+            branch = b
+    if not branch:
+        branch = Tree(word[0])
+        trie.branches.append(branch)
+    add_word(branch, word[1:])
+
+
+def get_words(trie):
+    """
+    >>> get_words(make_trie(['this', 'is', 'the', 'trie']))
+    ['this', 'the', 'trie', 'is']
+    """
+    if is_leaf(trie):
+        return [trie.label]
+    return sum( [[trie.label + word for word in get_words(b)] for b in trie.branches] , [])
